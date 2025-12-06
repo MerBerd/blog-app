15df6db4587cf68ce9c45ffd935cd93c146783b1
mergen <mergen00fub@gmail.com>
2025-12-06 13:58:31 +0500

    added db schema for migrations

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/MerBerd/blog-app/internal/handlers"
 	"github.com/MerBerd/blog-app/internal/repositories"
 	"github.com/MerBerd/blog-app/internal/services"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("failed to load configs: %s", err.Error())
+	}
+
 	repos := repositories.NewRepository()
 	services := services.NewService(repos)
 	handlers := handlers.NewHandler(services)
 
 	srv := &server.Server{}
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("failed to run server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE attachments;
+
+DROP TABLE comments;
+
+DROP TABLE articles;
+
+DROP TABLE users;
+
+
+
+
+
+
